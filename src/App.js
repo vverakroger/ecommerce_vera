e18df5667061150bc426b51c01dcc3b21407b7cf
vverakroger <valeria2251997@gmail.com>
2022-01-12 21:14:21 -0300

    Initial Commit Repo Creation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import NavBar from './components/NavBar/NavBar';
+import logo from './logo.svg';
 import './App.css';
-import ItemListContainer from './components/containers/ItemListContainer';
 
 function App() {
   return (
-    <div>
-      <NavBar />
-      <ItemListContainer />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         HOLA
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
