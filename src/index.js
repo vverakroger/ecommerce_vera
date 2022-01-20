f181eb6deb42404220b312136f362f8526f8103c
vverakroger <valeria2251997@gmail.com>
2022-01-20 20:32:18 -0300

    NavBar created 2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Nav from './components/navBar/NavBar';
+import Nav from './components/navbar/NavBar';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
